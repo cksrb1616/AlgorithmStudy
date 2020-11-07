fd5fe9c60f458037ffbb9de8ef5889d1b5d9453b
cksrb1616 <53897355+cksrb1616@users.noreply.github.com>
2020-11-07 09:06:36 +0100

    Create mid1mj.py

---
+++ 07NOV/mid1mj.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    arrpos = [*enumerate(arr)]
+    arrpos.sort(key = lambda it : it[1])
+    vis = {k : False for k in range(n)}
+    ans = 0
+    
+    for i in range(n):
+        if vis[i] or arrpos[i][0] == i:
+            print('continue'+str(i))
+            continue
+        cycle_size = 0
+        j = i
+         
+        while not vis[j]:
+            vis[j] = True
+            j = arrpos[j][0]
+            cycle_size += 1
+
+        if cycle_size > 0:
+            ans += (cycle_size - 1)
+    return ans
